51b92b5503b43e33b70d89346abb2b7238dc37ec
Atef Badri <atefbedri@gmail.com>
2023-11-05 12:17:24 +0100

    implement static interface for stats

--- src/app/pages/results/stats/stats.component.ts
+++ src/app/pages/results/stats/stats.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stats.component.scss']
 })
 export class StatsComponent implements OnInit {
-
+  abilities = [
+    { name: 'HP', percent: 80 },
+    { name: 'ATK', percent: 70 },
+    { name: 'DEF', percent: 60 },
+    { name: 'SATK', percent: 90 },
+  ];
   constructor() { }
 
   ngOnInit(): void {
