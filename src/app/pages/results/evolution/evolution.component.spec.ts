1c6e89a120e2451007112e4f7e82c687386a550f
Atef Badri <badri.atef@neopolis-dev.com>
2023-11-06 20:54:26 +0100

    write test for EvolutionComponent

--- src/app/pages/results/evolution/evolution.component.spec.ts
+++ src/app/pages/results/evolution/evolution.component.spec.ts
@@ -22,4 +22,24 @@ describe('EvolutionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render evolutions with images and names', () => {
+    const evolutions = [
+      { name: 'pikachu', image: 'pikachu.png' },
+      { name: 'raichu', image: 'raichu.png' }
+    ];
+
+    component.evolutions = evolutions;
+    fixture.detectChanges();
+
+    const evolutionElements = fixture.nativeElement.querySelectorAll('.evolution');
+    expect(evolutionElements.length).toBe(1); // one image should be rendered
+
+    const imageElements = fixture.nativeElement.querySelectorAll('.evolution-image img');
+    expect(imageElements.length).toBe(2); // Two images should be rendered
+
+    const nameElements = fixture.nativeElement.querySelectorAll('.evolution-image p');
+    expect(nameElements.length).toBe(2); // Two names should be rendered
+
+  });
 });
