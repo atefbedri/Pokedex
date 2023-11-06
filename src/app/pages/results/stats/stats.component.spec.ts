bb2bc60efa0d1b1e4e999587422a80d5626605dd
Atef Badri <badri.atef@neopolis-dev.com>
2023-11-06 21:06:33 +0100

    write test for StatsComponent

--- src/app/pages/results/stats/stats.component.spec.ts
+++ src/app/pages/results/stats/stats.component.spec.ts
@@ -22,4 +22,27 @@ describe('StatsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render stats with names, values, and progress bars', () => {
+    const stats = [
+      { name: 'HP', value: 80 },
+      { name: 'ATK', value: 65 },
+      { name: 'DEF', value: 70 }
+    ];
+
+    component.stats = stats;
+    fixture.detectChanges();
+
+    const abilityElements = fixture.nativeElement.querySelectorAll('.ability');
+    expect(abilityElements.length).toBe(3); // 3 abilities should be rendered
+
+    const nameElements = fixture.nativeElement.querySelectorAll('.ability-name');
+    expect(nameElements.length).toBe(3); // 3 ability names should be rendered
+
+    const valueElements = fixture.nativeElement.querySelectorAll('.ability-value');
+    expect(valueElements.length).toBe(3); // 3 ability values should be rendered
+
+    const progressBarElements = fixture.nativeElement.querySelectorAll('.progress-bar');
+    expect(progressBarElements.length).toBe(3); // 3 progress bars should be rendered
+  });
 });
