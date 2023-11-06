983441939994b9c7d6dbd98c3647ec37bd9d481d
Atef Badri <badri.atef@neopolis-dev.com>
2023-11-06 21:19:50 +0100

    write test for ResultsComponent

--- src/app/pages/results/results.component.spec.ts
+++ src/app/pages/results/results.component.spec.ts
@@ -22,4 +22,25 @@ describe('ResultsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize with default values', () => {
+    expect(component.currentTab).toBe('STATS');
+    expect(component.pokemonDetails).toBeNull();
+    expect(component.pokemonData).toEqual({});
+    expect(component.evolutionImageUrl).toEqual("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/");
+  });
+
+  it('should call fetchPokemonDetails when getPokemonDetails is called', () => {
+    spyOn(component, 'fetchPokemonDetails');
+    const pokemonDetails = { name: 'pikachu' };
+    component.getPokemonDetails(pokemonDetails);
+    expect(component.pokemonDetails).toEqual(pokemonDetails);
+    expect(component.fetchPokemonDetails).toHaveBeenCalled();
+  });
+
+  it('should reset the component', () => {
+    spyOn(window.location, 'reload');
+    component.reset();
+    expect(window.location.reload).toHaveBeenCalled();
+  });
 });
